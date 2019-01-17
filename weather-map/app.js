7723e542e485859a0ae218ead6fc1b99f67aa3f1
zh-konopleova <konopleova@gmail.com>
2019-01-17 15:54:48 +0300

    Add geolocation (setCenter on map)

--- weather-map/app.js
+++ weather-map/app.js
@@ -2,36 +2,41 @@ const WEATHER_API_KEY = '08cfcbb48598e2c1c8f25175cff6e48c';
 
 ymaps.ready(init);
 
-function init(){
-    let myMap = new ymaps.Map("map", {
-        center: [53.9, 27.57],
-        zoom: 10
-    });
+function init() {
+  let myMap = new ymaps.Map("map", {
+    center: [53.9, 27.6],
+    zoom: 10
+  });
 
-    myMap.events.add('click', function (e) {
-        let coords = e.get('coords');
-        let response = requestWeatherAPI(coords[0], coords[1]);
-        let converter = {
-          'Clouds': 'cloudy',
-          'Snow': 'snowy',
-          'Rain': 'rainy',
-          'Sun': 'sunny',
-          'Clear': 'sunny'
-        };
-        icon = converter[response.weather[0].main] || 'another';
+  let gps = navigator.geolocation.getCurrentPosition((position) => {
+    myMap.setCenter([position.coords.latitude, position.coords.longitude]);
+  });
 
-        myMap.balloon.open(coords, {
-          contentHeader: response.name,
-          contentBody: getBaloonContentBody(response.main.temp, icon)
-        });
+  myMap.events.add('click', (e) => {
+    let coords = e.get('coords');
+    let response = requestWeatherAPI(coords[0], coords[1]);
+    let converter = {
+      'Clouds': 'cloudy',
+      'Snow': 'snowy',
+      'Rain': 'rainy',
+      'Sun': 'sunny',
+      'Clear': 'sunny'
+    };
+
+    icon = converter[response.weather[0].main] || 'another';
+
+    myMap.balloon.open(coords, {
+      contentHeader: response.name,
+      contentBody: getBaloonContentBody(response.main.temp, icon)
     });
+  });
 };
 
 function getBaloonContentBody(temp, icon) {
   return  `
     <div style="margin: 10px; display: flex; flex-direction: row; justify-content: space-between; align-items: center">
-        <img src="img/${icon}.svg" width="40px" height="40px">
-        <span style="padding-left: 5px; font-size: 20px; color: red">${temp}&#8451;</span>
+      <img src="img/${icon}.svg" width="40px" height="40px">
+      <span style="padding-left: 5px; font-size: 20px; color: red">${temp}&#8451;</span>
     </div>
   `
 };
@@ -48,4 +53,4 @@ function requestWeatherAPI(lat, lon) {
   } else {
     return JSON.parse(xhr.responseText);
   }
-}
+};
